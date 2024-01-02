641ad7de5a918c8b30742a924a0734eb4294c74f
Veronica Ni <vni1294@gmail.com>
2024-01-01 21:30:34 -0500

    Pseudocoded out solution to showNumberAll

--- asyExercise.js
+++ asyExercise.js
@@ -4,7 +4,7 @@ const BASE_URL = "http://numbersapi.com"
 
 /*****************************************************************************
  * Get trivia from Numbers API on a number
- *  Logs the JSON response from API.
+ *  Logs the trivia text from API response.
  **/
 async function showNumberTrivia(favNum){
 
@@ -21,7 +21,7 @@ async function showNumberTrivia(favNum){
 
 /*****************************************************************************
  * Get trivia from Numbers API on a number (via query param)
- *  Logs the JSON response from API.
+ *  Logs the trivia text from API response.
  **/
 async function showNumberTriviaQuery(favNum){
 
@@ -35,7 +35,8 @@ async function showNumberTriviaQuery(favNum){
 /*****************************************************************************
  * Makes 4 separate requests for trivia about 4 different numbers from
  *  Numbers API.
- *  Logs the JSON response from API.
+ *  Logs the trivia text from API response for the first number with a fulfilled
+ *  promise.
  **/
 async function showNumberRace(nums){
 
@@ -47,3 +48,31 @@ async function showNumberRace(nums){
 
   console.log(winningData.text);
 }
+
+/*****************************************************************************
+ * Makes 4 separate requests for trivia about 4 different numbers from
+ *  Numbers API.
+ *  Logs an array of responses for all four numbers once promises
+ *  are fulfilled.
+ **/
+async function showNumberAll(nums){
+  
+  const allRespPromises = nums.map(num => fetch(`${BASE_URL}/${num}?json`));
+
+  const data = await Promise.allSettled(allRespPromises);
+
+  console.log(data); //
+
+  const jsonAll = data.map(d => d.json());
+  console.log(jsonAll);
+  //data.map(d => { json = d.json
+    //if d.status is 200, then d.text, push to success
+    //else, d.value.statusText("Not Found"), push to failures
+  //})
+
+}
+
+//["the number 2 is ...", "the num 12 is...", "the num 4 is..."]
+//["Not Found"]
+
+showNumberAll(["WRONG", 2, 12, 4])
